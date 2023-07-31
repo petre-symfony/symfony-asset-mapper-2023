2c873a882b97c1f9f8b48531241db438de93d91f
Petre Ro <petrero@gmail.com>
2023-07-31 20:33:32 +0300

    2.2 Writing Modern JavaScript  - Time to make things interesting! Back in app.js, copy the mix name. Let's create a class: class MixedVinyl, with a constructor and some properties. This uses the class syntax introduced in ES6, or ECMAScript 6... basically version "6" of JavaScript. You'll hear ES6 a lot because most modern features you're used to came from this version - released way back in 2015.
     In the describe() method, I'm leveraging string interpolation - another modern feature from ES6 - to return the string. Below, use this: const - yet another ES6 feature - mix = new MixedVinyl() and pass in the mix name and year. Finally, console.log(mix.describe()).
    
     Cool! This is the kind of code I like to write every day. Unfortunately, this is also the kind of code that browsers have historically choked on!
    
     So, normally, we would have a build system like Encore that would read this modern code and rewrite it to old JavaScript... so it would work in our browser. But... tada! It already works in our browser! We don't need to do anything. And that's not just because I'm using a new browser. This is going to work in every browser.
    
     If you're ever unsure, go to https://caniuse.com to check it out. Let's look up "ES6 class". Yup, it's basically supported by everything... except for IE 11, which is dead.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-console.log('Awesome Mix Vol. 1');
+class MixedVinyl {
+  constructor(title, year) {
+    this.title = title;
+    this.year = year;
+  }
+
+  describe() {
+    return `${this.title} was released in ${this.year}`;
+  }
+}
+
+const mix = new MixedVinyl('Awesome Mix Vol. 1', 2014);
+console.log(mix.describe());
