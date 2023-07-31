52cd04e3255b512c7bb5127e1ed7ccc7c1c7d4ab
Petre Ro <petrero@gmail.com>
2023-07-31 20:40:20 +0300

    2.3 Using "import" in the Browser  Back over in app.js, import MixedVinyl from and, just like we do in Encore, use the relative path: ./vinyl.js.
     Though, notice that I am including the .js file extension... which you can do in Encore, but it's not required. More on that later - but this was on purpose.
    
     So... does my browser support the import statement? Let's find out! Refresh. Booo:
    
      Cannot use import statement outside a module

--- public/app.js
+++ public/app.js
@@ -1,13 +1,4 @@
-class MixedVinyl {
-  constructor(title, year) {
-    this.title = title;
-    this.year = year;
-  }
-
-  describe() {
-    return `${this.title} was released in ${this.year}`;
-  }
-}
+import MixedVinyl from './vinyl.js';
 
 const mix = new MixedVinyl('Awesome Mix Vol. 1', 2014);
 console.log(mix.describe());
