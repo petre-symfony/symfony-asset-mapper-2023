242c4f774eb20b29e9b2675b97d715d63083feef
Petre Ro <petrero@gmail.com>
2023-07-31 21:02:51 +0300

    2.6 What Features are Missing?  - However, there are two remaining problems. First, importing packages using the full URL is annoying. I want to be able to say import from 'lodash' The second problem is asset versioning. To have a performant system, we need the final files downloaded by the browser to have version hashes in their filenames, like app.1234abcd.js. We need this so that we can instruct browsers to perform long-term caching. And we can't get this by creating & serving files directly from public/.
     These are precisely the two things that Symfony's new AssetMapper component will help us solve. But I wanted to start with raw JavaScript so that we could see how... most of what we're doing is not solved by Symfony or AssetMapper or AI: it's solved by your browser and the modern web.
    
     Ok, let's delete these two files so I don't get confused... and also remove the import inside of base.html.twig. Don't worry! We'll see all of that code in a different way soon.
    
     Next: Let's install AssetMapper and get it rocking

--- public/app.js
+++
@@ -1,5 +0,0 @@
-import MixedVinyl from './vinyl.js';
-import _ from 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm';
-
-const mix = new MixedVinyl('Awesome Mix Vol. 1', 2014);
-console.log(_.camelCase(mix.describe()));
