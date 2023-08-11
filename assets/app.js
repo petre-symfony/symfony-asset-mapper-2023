d212bb3ff20649c9486d8989c8c52ba08376dec4
Petre Ro <petrero@gmail.com>
2023-08-11 11:00:48 +0300

    10.1 importmap:require - 3rd Party JS Libs
     In our code, we get to use import statements with relative paths, ES6 classes: everything we're used to. It's business as usual. Except, how can we use third-party packages?
    
     As we saw earlier, we could import things via a full URL, like import _ from, and I'll paste in the CDN URL that we used earlier. With that done, the rest is normal: add _.camelCase() to the log.
    
     If we refresh and check the console... that works. But I don't like it! I don't want to have to include this crazy URL everywhere I use lodash. And what happens if we upgrade lodash... and I need to change the URL in 10 different files? Lame!

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 import Vinyl from './lib/vinyl.js'
+import _ from 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm'
 
 const mix = new Vinyl('Awesome Mix Vol. 1', 2014);
 
-console.log(mix.describe());
+console.log(_.camelCase(mix.describe()));
