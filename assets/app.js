8f6369a1887fcdb6eb6dc5fc61c89605ce22d7ed
Petre Ro <petrero@gmail.com>
2023-08-11 11:24:15 +0300

    11.1 Importing Specific Package Files
     Sometimes, instead of importing a package itself, you may want to import only part of it: like a specific file. Lodash is a good example of this.
    
     No Tree Shaking in the Browser
     - But before we get there, instead of importing everything from lodash, you should be able to say import { camelCase } from 'lodash'. Then, down here, you would use camelCase directly.
    
     However, when we move over and try this... error!
    
      The requested module lodash does not provide an export named camelCase.
    
     This should work... and the reason it doesn't is complicated. Basically, due to the way that this specific library packages their module, you can't import specific functions like this. It will work with most other packages, however.
    
     For example, if you say import { Modal } from 'bootstrap' (if you're using Bootstrap), that will work. Bootstrap packages their files correctly.
    
     However, using this syntax may not always be ideal with AssetMapper.
    
     Here's the problem. If we ran this code through Encore, Encore would do something called "tree shaking". This is where it would see that we're only importing camelCase from lodash. And so, in the final JavaScript, it would only give us the code for camelCase, not the entire lodash package.
    
     In a browser environment, if you import from lodash, you're going to get all of lodash... even if you're only importing one part of it. Now, that might not be that big of a deal. The full build of lodash is still only 24 kilobytes. But what if we are using a big package... but only need to import one specific thing?

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 import Vinyl from './lib/vinyl.js'
-import _ from 'lodash'
+import { camelCase} from 'lodash'
 
 const mix = new Vinyl('Awesome Mix Vol. 1', 2014);
 
-console.log(_.camelCase(mix.describe()));
+console.log(camelCase(mix.describe()));
