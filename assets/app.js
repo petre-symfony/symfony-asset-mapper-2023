8e2039658aa1458b504c25197d39c4d5ebadf432
Petre Ro <petrero@gmail.com>
2023-09-29 18:11:36 +0300

    15.4 Seeing a List of Invalid Paths  - By the way, there's an almost hidden way to see if any invalid imports appear anywhere in your code.    First, run:
      php bin/console cache:clear
     That clears Symfony's cache, of course, but it also clears an internal cache in AssetMapper. Now when we run
    
      php bin/console debug:asset
     it re-builds the cache for all of those assets internally. When it does that, it parses our files and reports any missing imports. See?
    
      WARNING Unable to find asset ./lib/vinyl imported from assets/app.js.
    
     And in this case, we even get an extra message:
    
      Try adding ".js" to the end of the import
    
     Good idea. If we add that .js back... things work again.

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 import './bootstrap.js';
-import Vinyl from './lib/vinyl'
+import Vinyl from './lib/vinyl.js'
 import camelCase from 'lodash/camelCase'
 
 const mix = new Vinyl('Awesome Mix Vol. 1', 2014);
